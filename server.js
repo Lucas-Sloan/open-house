a0eed463b7ae4004579bf3f231afc076fbfcd31b
Lucas-Sloan <Lucasg.sloan@gmail.com>
2024-07-31 13:57:43 -0500

    Finished the exercises

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// server.js
 const dotenv = require('dotenv');
 dotenv.config();
 const express = require('express');
@@ -6,8 +7,11 @@ const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 const morgan = require('morgan');
 const session = require('express-session');
+const isSignedIn = require('./middleware/is-signed-in.js');
+const passUserToView = require('./middleware/pass-user-to-view.js');
 
 const authController = require('./controllers/auth.js');
+const listingsController = require('./controllers/listings.js');
 
 const port = process.env.PORT ? process.env.PORT : '3000';
 
@@ -16,6 +20,8 @@ mongoose.connect(process.env.MONGODB_URI);
 mongoose.connection.on('connected', () => {
   console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
 });
+
+app.set('view engine', 'ejs');
 
 app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride('_method'));
@@ -28,21 +34,15 @@ app.use(
   })
 );
 
+app.use(passUserToView);
+app.use('/auth', authController);
+app.use('/listings', isSignedIn, listingsController);
+
 app.get('/', (req, res) => {
   res.render('index.ejs', {
     user: req.session.user,
   });
 });
-
-app.get('/vip-lounge', (req, res) => {
-  if (req.session.user) {
-    res.send(`Welcome to the party ${req.session.user.username}.`);
-  } else {
-    res.send('Sorry, no guests allowed.');
-  }
-});
-
-app.use('/auth', authController);
 
 app.listen(port, () => {
   console.log(`The express app is ready on port ${port}!`);
